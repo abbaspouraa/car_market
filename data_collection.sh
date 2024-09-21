05b1369d526d48e61cb0501f3fee437d9ff8a125
abbaspouraa <amir@schemaapp.com>
2024-09-21 19:47:45 -0400

    data collection program

---
+++ data_collection.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+execution_number=0
+  # Wait time between checks in seconds.
+
+while true; do
+  wait_time=3600
+  # Get the current hour
+  current_hour=$(date +%H)
+
+  # Check if the current hour is between 6 AM and 12 AM
+  if [ "$current_hour" -ge 6 ] && [ "$current_hour" -lt 24 ]; then
+  execution_number=$((execution_number + 1))
+  python3 main.py
+  python3 db_process.py
+  fi
+  echo "------------------------ Execution: $execution_number. Wait time $wait_time. --------------------------"
+  sleep $wait_time
+done
