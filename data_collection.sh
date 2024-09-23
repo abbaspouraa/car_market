1555dab4a378e428c345ef56da2995ceb985486b
abbaspouraa <amir@schemaapp.com>
2024-09-23 15:11:47 -0400

    Update data_collection.sh

--- data_collection.sh
+++ data_collection.sh
@@ -11,12 +11,13 @@ while true; do
   # Check if the current hour is between 6 AM and 12 AM
   if [ "$current_hour" -ge 6 ] && [ "$current_hour" -lt 24 ]; then
     execution_number=$((execution_number + 1))
-    echo "------------------------ Execution: $execution_number. Wait time $wait_time. --------------------------"
+    echo "------------------------ Execution: $execution_number --------------------------"
     python3 main.py
     python3 db_process.py
     if [ "$current_hour" -ge 23 ] && [ "$current_hour" -lt 24 ]; then
       python3 identify_sold.py
     fi
   fi
+  echo "------------------------ Finished execution. Wait $wait_time seconds ------------------------"
   sleep $wait_time
 done
