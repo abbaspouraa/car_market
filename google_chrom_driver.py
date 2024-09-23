00ca12fe01b4cc84381c6a3c2ddd41d52397f574
abbaspouraa <amir@schemaapp.com>
2024-09-23 12:42:17 -0400

    switch to Headless Chrome

--- google_chrom_driver.py
+++ google_chrom_driver.py
@@ -2,17 +2,13 @@ from selenium import webdriver
 import undetected_chromedriver as uc
 
 def get_driver() -> webdriver.Chrome:
-    # Initialize Chrome options
-    prefs = {
-        'profile.managed_default_content_settings.images': 2,
-        'disk-cache-size': 4096
-    }
-
     # Adding preferences to Chrome options
     options = webdriver.ChromeOptions()
     user_agent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36"
     options.add_argument(f"user-agent={user_agent}")
-    options.add_experimental_option('prefs', prefs)
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--window-size=1920,1080')
 
     # Initialize the WebDriver with the ChromeDriverManager
-    return uc.Chrome(version_main=127)
+    return uc.Chrome(options=options, version_main=127)
