de9f07cbda5afba0cc8c0abb4be6d5ae8a009736
abbaspouraa <amir@schemaapp.com>
2024-09-24 23:27:39 -0400

    Update CarImage.sql

--- DDL/CarImage.sql
+++ DDL/CarImage.sql
@@ -4,7 +4,9 @@ CREATE TABLE IF NOT EXISTS CarImage (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     car_url_id BIGINT UNSIGNED NOT NULL,
     imageUrl VARCHAR(1000) NOT NULL,
-    hash_512 CHAR(128) GENERATED ALWAYS AS (sha2(imageUrl, 512)) STORED,
-    UNIQUE KEY `unique_images` (hash_512),
+    image_id VARCHAR(255) GENERATED ALWAYS AS (
+        SUBSTRING_INDEX(SUBSTRING_INDEX(SUBSTRING_INDEX(imageUrl, '/', -1), '?', 1), '.', 1)
+    ) STORED,
+    UNIQUE KEY `unique_images` (image_id),
     FOREIGN KEY (car_url_id) REFERENCES CarWarehouse(url_id) ON DELETE CASCADE
 );
