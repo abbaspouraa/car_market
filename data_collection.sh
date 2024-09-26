608ab0d9a69688cb4ac9ea4a71f05b001e75855a
abbaspouraa <amir@schemaapp.com>
2024-09-26 16:37:25 -0400

    send emials when finding good results

--- data_collection.sh
+++ data_collection.sh
@@ -4,17 +4,20 @@ execution_number=0
   # Wait time between checks in seconds.
 
 while true; do
-  wait_time=3600
+  wait_time=1800
   # Get the current hour
   current_hour=$(date +%H)
+  current_minute=$(date +%M)
 
   # Check if the current hour is between 6 AM and 12 AM
-  if [ "$current_hour" -ge 6 ] && [ "$current_hour" -lt 24 ]; then
+  if [ "$current_hour" -ge 6 ]; then
     execution_number=$((execution_number + 1))
     echo "------------------------ Execution: $execution_number --------------------------"
     python3 main.py
     python3 db_process.py
-    if [ "$current_hour" -ge 23 ] && [ "$current_hour" -lt 24 ]; then
+    python3 send_email.py
+    if [ "$current_hour" -ge 23 ] && [ "$current_minute" -ge 30 ]; then
+      # Only run once a day
       python3 identify_sold.py
     fi
   fi
