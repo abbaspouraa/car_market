01ba6ad64f99e921533f9581c5a157e5fb68cc88
abbaspouraa <amir@schemaapp.com>
2024-09-29 10:39:28 -0400

    Update google_chrom_driver.py

--- google_chrom_driver.py
+++ google_chrom_driver.py
@@ -5,8 +5,8 @@ def get_driver() -> webdriver.Chrome:
     options = webdriver.ChromeOptions()
     user_agent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36"
     options.add_argument(f"user-agent={user_agent}")
-    # options.add_argument('--headless')  # Use '--headless' for standard headless mode
-    # options.add_argument('--disable-gpu')
+    options.add_argument('--headless')  # Use '--headless' for standard headless mode
+    options.add_argument('--disable-gpu')
     options.add_argument('--window-size=1920,1080')
 
     prefs = {
